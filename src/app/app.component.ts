5fd58c0c212ea832c66f54409ccca69ff6080625
nurjanahevs <evasitinurjanah2019@gmail.com>
2022-04-21 08:48:07 +0700

    this is the test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,76 @@
 import { Component } from '@angular/core';
+import { DataSource } from '@angular/cdk/collections';
+import { Observable, ReplaySubject } from 'rxjs';
+
+import mentors from './files/mentor.json';
+
+export interface PeriodicElement {
+  _id: string;
+  email: string;
+  civility: string;
+  first_name: string;
+  last_name: string;
+  company: {
+    name: string;
+    user_type: string;
+  };
+  user_status: string;
+  count_document: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = mentors
+
+console.log(ELEMENT_DATA);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-test';
+
+  displayedColumns: string[] = [
+    'name',
+    'user_type',
+    'entity',
+    'status',
+    'action'
+  ];
+  dataToDisplay = [...ELEMENT_DATA];
+
+  dataSource = new ExampleDataSource(this.dataToDisplay);
+
+  addData() {
+    const randomElementIndex = Math.floor(Math.random() * ELEMENT_DATA.length);
+    this.dataToDisplay = [
+      ...this.dataToDisplay,
+      ELEMENT_DATA[randomElementIndex],
+    ];
+    this.dataSource.setData(this.dataToDisplay);
+  }
+
+  removeData() {
+    this.dataToDisplay = this.dataToDisplay.slice(0, -1);
+    this.dataSource.setData(this.dataToDisplay);
+  }
+}
+
+class ExampleDataSource extends DataSource<PeriodicElement> {
+  private _dataStream = new ReplaySubject<PeriodicElement[]>();
+
+  constructor(initialData: PeriodicElement[]) {
+    super();
+    this.setData(initialData);
+  }
+
+  connect(): Observable<PeriodicElement[]> {
+    return this._dataStream;
+  }
+
+  disconnect() {}
+
+  setData(data: PeriodicElement[]) {
+    this._dataStream.next(data);
+  }
 }
